27d498ed2544a0dbd8af27de73cbdfc18b0ea0b8
Jirasak07 <bee.110243@gmail.com>
2022-11-16 10:31:01 +0700

    edit headers

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ var upload = multer({
   fileFilter:isImage
 })
 
-app.use(cors());
+// app.use(cors());
 app.use((req, res,next)=>{
   res.header('Access-Control-Allow-Origin','*');
   res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
