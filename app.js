737915b0a8328a3a63247a9f87460e71d78f2723
Jirasak07 <bee.110243@gmail.com>
2022-11-16 10:28:47 +0700

    edit headers

--- app.js
+++ app.js
@@ -37,6 +37,11 @@ var upload = multer({
 })
 
 app.use(cors());
+app.use((req, res,next)=>{
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
 app.use("/img",express.static("./asset/img"))
 const mysql = require("mysql2");
 
